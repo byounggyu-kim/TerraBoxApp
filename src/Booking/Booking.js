84c89b585dce0af18ce175afd27401c37de7748a
byounggyu-kim <briankim0916@gmail.com>
2022-07-05 16:37:31 +0900

    Add: 예매페이지 영화리스트 레이아웃 및 버튼 작업중

--- src/Booking/Booking.js
+++ src/Booking/Booking.js
@@ -1,10 +1,12 @@
-import {StyleSheet, Text, View, FlatList} from 'react-native';
+import {StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import MovieList from '../Components/Booking/MovieList';
 
 const Booking = () => {
   const [movieNames, setMovieNames] = useState([]);
   const [regionLists, setRegionLists] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState({});
+  const [btnActive, setBtnActive] = useState(true);
 
   useEffect(() => {
     fetch('http://15.164.163.31:8000/Reserve/movie')
@@ -18,10 +20,23 @@ const Booking = () => {
       .then(data => setRegionLists(data.regions));
   }, []);
 
-  const renderItem = ({item}) => <MovieList item={item} />;
+  useEffect(() => {
+    if (selectedMovie !== {} && selectedMovie) {
+      setBtnActive(false);
+    }
+  }, [selectedMovie]);
+
+  const renderItem = ({item}) => (
+    <MovieList
+      item={item}
+      selectedMovie={selectedMovie}
+      setSelectedMovie={setSelectedMovie}
+    />
+  );
 
   return (
     <View style={styles.white}>
+      <Text style={styles.movieSelect}>관람하실 영화를 선택하세요</Text>
       <View style={styles.table}>
         <FlatList
           data={movieNames}
@@ -29,6 +44,11 @@ const Booking = () => {
           keyExtractor={item => item.movie_id}
         />
       </View>
+      <TouchableOpacity
+        style={[styles.button, {opacity: !btnActive ? '0.3' : '1'}]}
+        disabled={btnActive}>
+        <Text>선택하기</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -42,10 +62,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
 
+  movieSelect: {
+    fontSize: 20,
+    fontWeight: '600',
+    marginTop: 50,
+  },
+
   table: {
-    marginTop: 100,
-    borderWidth: 1,
+    marginTop: 50,
+    borderWidth: 0.5,
+    // borderRadius: 20,
     width: '90%',
-    height: '80%',
+    height: '60%',
+  },
+
+  button: {
+    width: '30%',
+    height: '20%',
+    backgroundColor: 'red',
   },
 });
--- src/Components/Booking/MovieList.js
+++ src/Components/Booking/MovieList.js
@@ -1,14 +1,41 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
 
-const MovieList = ({item}) => {
+const MovieList = ({item, selectedMovie, setSelectedMovie, setBtnActive}) => {
+  const buttonActivation = obj => {
+    setSelectedMovie(obj);
+  };
+
+  const yes = selectedMovie === item;
+
   return (
-    <View>
-      <Text>MovieList</Text>
-    </View>
+    <TouchableOpacity
+      style={[styles.singleMovie, {backgroundColor: yes ? 'gray' : 'white'}]}
+      onPress={() => buttonActivation(item)}>
+      <Image source={{uri: item.age_grade}} style={styles.age} />
+      <Text style={[styles.movieName, {color: yes ? 'white' : 'black'}]}>
+        {item.movie_name}
+      </Text>
+    </TouchableOpacity>
   );
 };
 
 export default MovieList;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  singleMovie: {
+    flexDirection: 'row',
+    padding: 20,
+  },
+
+  age: {
+    width: 25,
+    height: 27,
+  },
+
+  movieName: {
+    fontSize: 16,
+    fontWeight: '500',
+    marginLeft: 15,
+  },
+});
